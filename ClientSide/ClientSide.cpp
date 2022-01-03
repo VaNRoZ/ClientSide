933082accc6d85379844d4393e499b936e26d2fe
VaNRoZ <giga1345@gmail.com>
2022-01-03 21:48:06 +0200

    Добавьте файлы проекта.

---
+++ ClientSide/ClientSide.cpp
@@ -0,0 +1,169 @@
+﻿#include "Cliet.h"
+#include "Protocol.h"
+
+const string MSG_FROM_SERVER = "Got msg form server: ";
+const string EXIT_SESSION = "to exit current session or chat room enter cs\n";
+
+void print_instructions(;
+int main() {
+
+	string command, username, password, roomName;
+	string ip, peerUsr, msg;
+	Clien client;
+
+	cout << "Welcome Dear User|nthis is your new messenger\nPlease enter a command by the following:" << endl;
+
+	print_instructions();
+
+	//waiting for commands from cli
+	do
+	{
+		command, clear();
+		cin >> command;
+
+		if (command == "c")//connect to server
+		{
+			cin >> ip;
+			if (client, connectToServer(ip, SERVER_PORT) == false)
+			{
+				cout << "Could not connect to server" << endl;
+			}
+			else
+			{
+				client.isConnectedToServer = true;
+			}
+			continue;
+		}
+		else if (command == "login")//login to server
+		{
+			cin >> username;
+			cin >> password;
+			if (client.isConnectedToServer == false)
+			{
+				cout << "ERROR = You are not connected to a server" << endl;
+				continue;
+			}
+			else if (client, isLoggedIn == false)
+			{
+				client.login(username, password);
+				sleep(5);
+
+			}
+			else
+				cout << "you are not connected to a server" << endl;
+
+		}
+
+		else if (command == "register") // signup to server
+		{
+			if (cliet.isConnectedToServer == false)
+			{
+				cout << "ERROR - You are not conncted to a server" << endl;
+				continue;
+			}
+			cin >> username;
+			cin >> password;
+			client.signup(username, password);
+		}
+		else if (command == "lu") //list all users form server
+		{
+			if (client.isConnectedToServer == false)
+			{
+				cout << "ERROR - You are not connected to a server" << endl;
+				continue;
+			}
+			client.getUsers();
+		}
+		else if (command == "luc")//list all connected users
+		{
+			if (client.isConnectedToServer == false)
+			{
+				cout << "ERROR - You are not connected to a server" << endl;
+				continue;
+			}
+			client.listAllUsers();
+		}
+		else if (command == "lr") //list all chatrooms from server
+		{
+			if (client.isConnectedToServer == false)
+			{
+				cout << "ERROR - You are not connected to a server" << endl;
+				continue;
+			}
+			client.listAllChatrooms();
+		}
+		else if (command == "lru") //print a chatroom's user list
+		{
+			if (client.isConnectedToServer == false)
+			{
+				cout << "ERROR - You are not connected to a server" << endl;
+				continue;
+			}
+			cin >> roomName;
+			client.listChatroomUsers(roomName);
+		}
+		else if (command == "o") //opena session with a user
+		{
+			cin >> peerUsr;
+			if (client.isConnectedToServer == false)
+			{
+				cout << "ERROR - You are not connected to a server" << endl;
+				continue;
+			}
+			else if (client.isInSession == true)
+			{
+				cout<<"ERROR - already in session with other user or room" << EXIT_SESSION << endl;
+				continue;
+			}
+			else
+			{
+				client.openSession(username, peerUsr);
+			}
+		}
+		else if (command == "cs") //close the current session/chatroom
+		{
+			if (client.isConnectedToServer == false)
+			{
+				cout << "ERROR - You are not connected to a server" << endl;
+				continue;
+			}
+			client.closeSession();
+
+		}
+		else if (command == "or")// create (open) a new chatroom
+		{
+			cin >> roomName;
+			if (client.isConnectedToServer == false)
+			{
+				cout << "ERROR - You are not connected to a server" << endl;
+				continue;
+			}
+			else if (client.isInSession == true)
+			{
+				cout << "ERROR - afready in session wif other user or room" << EXIT_SESSION << endl;
+			}
+			else
+			{
+				client.CreateChatRoom(roomName);
+				client.charName = roomName;
+			}
+		}
+		else if (command == "s") //send a msg to user in session
+		{
+		if (client.isconnctedToServer == false)
+		{
+			cout << "ERROR - You are not connected to a server" << endl;
+			continue;
+		}
+		else if (client.isInSession == true) 
+			{
+			getline(std::cin, msg);
+			if (msg.size() > 0 && msg[0] == ' ')
+				msg.erase(0, 1);
+			client.sendMsgToSession(msg);
+			}
+			else
+			cout << "error" << endl; // line 172
+		}
+	}
+}
